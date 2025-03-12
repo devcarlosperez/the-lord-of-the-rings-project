b509e88e646b5c22f2f3d09ec59e7105dcbbd2d0
devcarlosperez <carlosperezsantana04@gmail.com>
2025-03-12 22:58:05 +0000

    connected firebase SDK to the project

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCWWxy57Rj1Hsj6hp4tt0CJWI_2XN_Dt14",
+  authDomain: "lord-of-the-rings-4444a.firebaseapp.com",
+  projectId: "lord-of-the-rings-4444a",
+  storageBucket: "lord-of-the-rings-4444a.firebasestorage.app",
+  messagingSenderId: "107290559503",
+  appId: "1:107290559503:web:52b8cc6d838b241d198f7a"
+};
+
+const APP = initializeApp(FIREBASE_CONFIG);
