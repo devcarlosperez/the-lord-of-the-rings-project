e748e7ae9b070b65e9c7fb3053f9b5a47033c56c
devcarlosperez <carlosperezsantana04@gmail.com>
2025-03-12 22:40:21 +0000

    add Firebase Hosting

--- src/components/map-hobbiton/HobbitonMap.jsx
+++ src/components/map-hobbiton/HobbitonMap.jsx
@@ -1,8 +1,18 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "./HobbitonMap.css";
 
 function HobbitonMap() {
   const POSITION = [-37.869383, 175.68195];
+
+  const MARKER_ICON = new L.Icon({
+    iconUrl: "/images/marker-icon.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: "/images/marker-shadow.png",
+    shadowSize: [41, 41]
+  });
 
   return (
     <MapContainer
@@ -15,7 +25,7 @@ function HobbitonMap() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={POSITION}>
+      <Marker position={POSITION} icon={MARKER_ICON}>
         <Popup>Welcome to Hobbiton!</Popup>
       </Marker>
     </MapContainer>
