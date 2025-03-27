2c41e42835ccfc5a4a88b279dc4c5fc39c04a96a
devcarlosperez <carlosperezsantana04@gmail.com>
2025-03-27 08:53:38 +0000

    add hamburger button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,29 @@
+import { useState } from "react";
 import "./Header.css";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="header-container">
-      <a className="logo" href="/home">
-        <img className="logo-image" src="/images/logo.png" alt="logo" />
-        <h2 className="logo-name">The Lord of the Rings</h2>
-      </a>
-      <nav>
-        <a href="/home">Home</a>
-        <a href="/characters">Characters</a>
-        <a href="/hobbiton">Hobbiton</a>
-        <a href="/forum">Forum</a>
-      </nav>
-    </header>
+    <>
+      <button className="menu-button" onClick={() => setIsOpen(!isOpen)}>
+        <i className="bx bx-menu"/>
+      </button>
+
+      <header className={`header-container ${isOpen ? "show" : ""}`}>
+        <a className={`logo ${isOpen ? "hide" : ""}`} href="/home">
+          <img className="logo-image" src="/images/logo.png" alt="logo" />
+          <h2 className="logo-name">The Lord of the Rings</h2>
+        </a>
+        
+        <nav className={`${isOpen ? "show-menu" : ""}`}>
+          <a href="/home">Home</a>
+          <a href="/characters">Characters</a>
+          <a href="/hobbiton">Hobbiton</a>
+          <a href="/forum">Forum</a>
+        </nav>
+      </header>
+    </>
   );
 }
 
