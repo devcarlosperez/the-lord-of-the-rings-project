8b90f2bb5da1cfbdf601757bdf05b769c0486f9f
devcarlosperez <carlosperezsantana04@gmail.com>
2025-03-27 09:32:27 +0000

    add button delete comment in the forum

--- src/components/forum-list/ForumList.jsx
+++ src/components/forum-list/ForumList.jsx
@@ -1,40 +1,51 @@
 import { useEffect, useState } from "react";
-import { ref, onValue } from "firebase/database";
+import { ref, onValue, remove } from "firebase/database";
 import { DATABASE } from "../../firebase";
 import "./ForumList.css";
 
 function ForumList() {
-  const [topics, setTopics] = useState([]);
+  const [TOPICS, setTopics] = useState([]);
 
   useEffect(() => {
-    const topicsRef = ref(DATABASE, "topics");
+    const TOPICS_REF = ref(DATABASE, "topics");
 
-    onValue(topicsRef, (snapshot) => {
+    onValue(TOPICS_REF, (snapshot) => {
       if (snapshot.exists()) {
-        const data = snapshot.val();
-        const topicsArray = Object.keys(data).map((key) => ({
+        const DATA = snapshot.val();
+        const TOPICS_ARRAY = Object.keys(DATA).map((key) => ({
           id: key,
-          ...data[key],
+          ...DATA[key],
         }));
-        setTopics(topicsArray);
+        setTopics(TOPICS_ARRAY);
       }
     });
   }, []);
 
+  const handleDeleteComment = (TOPIC_ID, COMMENT_ID) => {
+    const COMMENT_REF = ref(
+      DATABASE,
+      `topics/${TOPIC_ID}/comments/${COMMENT_ID}`
+    );
+    remove(COMMENT_REF);
+  };
+
   return (
     <div className="forum-list">
-      {topics.map((topic) => (
-        <div key={topic.id} className="topic-card">
-          <h2>{topic.topic}</h2>
+      {TOPICS.map((TOPIC) => (
+        <div key={TOPIC.id} className="topic-card">
+          <h2>{TOPIC.topic}</h2>
           <div className="comments">
-            {Object.keys(topic.comments).map((commentId) => {
-              const COMMENT = topic.comments[commentId];
+            {Object.keys(TOPIC.comments).map((COMMENT_ID) => {
+              const COMMENT = TOPIC.comments[COMMENT_ID];
               return (
-                <div key={commentId} className="comment">
+                <div key={COMMENT_ID} className="comment">
                   <p>
                     <strong>{COMMENT.username}</strong>: {COMMENT.message}
                   </p>
                   <span>{COMMENT.date}</span>
+                  <button className="button-crud"
+                    onClick={() => handleDeleteComment(TOPIC.id, COMMENT_ID)}
+                  >DELETE COMMENT</button>
                 </div>
               );
             })}
