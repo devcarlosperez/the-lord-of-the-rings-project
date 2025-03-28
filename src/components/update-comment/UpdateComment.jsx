cb3cbcc4b0788906f5050626e424c375f8452dc7
devcarlosperez <carlosperezsantana04@gmail.com>
2025-03-28 12:35:30 +0000

    add form add comment and update comment

---
+++ src/components/update-comment/UpdateComment.jsx
@@ -0,0 +1,38 @@
+import "./UpdateComment.css";
+import * as React from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import TextField from "@mui/material/TextField";
+
+function UpdateComment() {
+  const [selectedDate, setSelectedDate] = React.useState(null);
+
+  return (
+    <form className="form-update-comment">
+      <div className="camps">
+        <div>
+          <label>Username</label>
+          <input type="text" name="username" id="username" />
+        </div>
+        <div>
+          <label>Message</label>
+          <input type="text" name="message" id="message" />
+        </div>
+        <div>
+          <label>Date</label>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              value={selectedDate}
+              onChange={(newValue) => setSelectedDate(newValue)}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
+        </div>
+      </div>
+      <button type="submit" className="button-submit">Enviar</button>
+    </form>
+  );
+}
+
+export default UpdateComment;
--- src/pages/form-add-comment/FormAddComment.jsx
+++ src/pages/form-add-comment/FormAddComment.jsx
@@ -1,36 +1,14 @@
-import "./FormAddComment.css";
-import * as React from "react";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import TextField from "@mui/material/TextField";
+import AddComment from "../../components/add-comment/AddComment";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
 
 function FormAddComment() {
-  const [selectedDate, setSelectedDate] = React.useState(null);
-
   return (
-    <form className="form-add-comment">
-      <div className="camps">
-        <div>
-          <label>Username</label>
-          <input type="text" name="username" id="username" />
-        </div>
-        <div>
-          <label>Message</label>
-          <input type="text" name="message" id="message" />
-        </div>
-        <div>
-          <label>Date</label>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              value={selectedDate}
-              onChange={(newValue) => setSelectedDate(newValue)}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
-        </div>
-      </div>
-    </form>
+    <>
+      <Header/>
+      <AddComment/>
+      <Footer/>
+    </>
   );
 }
 
